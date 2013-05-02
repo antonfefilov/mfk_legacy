1a9451ff4b8a24c393a483bbef66e64cde9e10cb
haul <h4ul@me.com>
2013-05-03 00:56:11 +0400

    add some attributes to the user and tests for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :username
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_USERNAME_REGEX = /\A[A-Za-z][A-Za-z0-9._-]{2,19}\z/
+  
+  validates :username, presence: true, format: { with: VALID_USERNAME_REGEX },
+                    uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false } 
+
+  before_save { |user| user.email = email.downcase }
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,90 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
+require 'spec_helper'
+
+describe User do
+  let (:user) { Fabricate.build(:user, username: "Alex", email: "alex@example.com")}
+  subject { user }
+
+  it { should respond_to(:username) } 
+  it { should respond_to(:email) } 
+  it { should respond_to(:password_digest) }
+
+  it { should be_valid }
+
+  describe "when username is not present" do
+    before { user.username = ""}
+    it {should_not be_valid}
+  end
+
+  describe "when email is not present" do
+    before { user.email = ""}
+    it {should_not be_valid}
+  end
+
+  describe "when username format is invalid" do
+    it "should be invalid" do
+      usernames = %w[al?ex mi=mi=no bo this_username_is_longer_than_20_characters]
+      usernames.each do |invalid_username|
+        user.username = invalid_username
+        user.should_not be_valid
+      end  
+    end
+  end
+
+  describe "when username format is valid" do
+    it "should be valid" do
+      usernames = %w[al_ex mi-mi.no bo567 shorter21characters]
+      usernames.each do |valid_username|
+        user.username = valid_username
+        user.should be_valid
+      end  
+    end
+  end
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        user.email = invalid_address
+        user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        user.email = valid_address
+        user.should be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_duplicated_email = user.dup
+      user_with_duplicated_email.email = user.email.upcase
+      user_with_duplicated_email.save
+    end
+    it { should_not be_valid} 
+  end
+
+  describe "when username is already taken" do
+    before do
+      user_with_duplicated_username = user.dup
+      user_with_duplicated_username.save
+    end
+    it { should_not be_valid }
+  end
+end
