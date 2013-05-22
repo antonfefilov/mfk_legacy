ca6a06b4996390ee08e23e8e62d96272d8549333
haul <h4ul@me.com>
2013-05-22 10:15:38 +0400

    add specs for index and new views

--- spec/views/admin/users/index.html.erb_spec.rb
+++ spec/views/admin/users/index.html.erb_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
-describe "users/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "admin/users/index.html.erb" do
+  it "displays all the users" do
+    assign(:users, [
+      stub_model(User, username: "Alex", email: "alex@example.com"),
+      stub_model(User, username: "Frodo", email: "frodo@example.com")
+    ]) 
+
+    render
+
+    rendered.should have_content("Frodo") 
+    rendered.should have_content("Alex") 
+    rendered.should have_content("alex@example.com") 
+    rendered.should have_content("frodo@example.com") 
+  end
 end
--- spec/views/admin/users/new.html.erb_spec.rb
+++ spec/views/admin/users/new.html.erb_spec.rb
@@ -1,5 +1,31 @@
+
 require 'spec_helper'
 
-describe "users/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "admin/users/new.html.erb" do
+  context "render the form for the new user creation" do
+    before(:each) do
+      assign(:user, stub_model(User))
+    end
+
+    it "have input field for a username" do
+      render
+      rendered.should have_selector("form input[@id='user_username']")
+    end
+    
+    it "have input field for a email" do
+      render
+      rendered.should have_selector("form input[@id='user_email']")
+    end
+    
+    it "have input field for a password" do
+      render
+      rendered.should have_selector("form input[@id='user_password']")
+    end
+    
+    it "have input field for a password confirmation" do
+      render
+      rendered.should have_selector("form input[@id='user_password_confirmation']")
+    end
+
+  end
 end
