1d4d1df9c7f763e566e7b57fc160b1d9321b0773
haul <h4ul@me.com>
2013-05-22 10:07:36 +0400

    add specs for testing requests for edit and show action

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -25,30 +25,17 @@ describe Admin::UsersController do
 
   describe "GET 'edit'" do
     it "returns http success" do
-      get 'edit'
+      user = Fabricate(:user, username: "Alex", email: "alex@example.com", password_digest: "anydigest") 
+      get 'edit', :id => user.id
       response.should be_success
     end
   end
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      user = Fabricate(:user, username: "Alex", email: "alex@example.com", password_digest: "anydigest") 
+      get 'show', :id => user.id
       response.should be_success
     end
   end
-
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
-      response.should be_success
-    end
-  end
-
 end
